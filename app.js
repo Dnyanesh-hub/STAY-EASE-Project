90d07d2ba89e7aa3840fe0eb38f93365f72676bb
Dnyaneshwar <dnyaneshwarmali0102@gmail.com>
2026-03-06 23:25:21 +0530

    implement sign up and log in functionality

--- app.js
+++ app.js
@@ -9,8 +9,9 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
 const Review = require("./models/review.js");
-const listings = require("./routes/listing.js");
-const reviews = require("./routes/review.js");
+const listingRouter = require("./routes/listing.js");
+const reviewRouter = require("./routes/review.js");
+const userRouter=require("./routes/user.js");
 const session = require("express-session");
 const flash = require("connect-flash");
 const passport = require("passport");
@@ -63,17 +64,18 @@ app.use((req, res, next) => {
   next();
 });
 //register route
-app.get("/demouser", async (req, res) => {
-  let fakeUser = new User({
-    email: "student@gmail.com",
-    username: "delta-student",
-  });
-  let registerdUser= await User.register(fakeUser,"helloworld");
-  res.send(registerdUser);
-});
+// app.get("/demouser", async (req, res) => {
+//   let fakeUser = new User({
+//     email: "student@gmail.com",
+//     username: "delta-student",
+//   });
+//   let registerdUser= await User.register(fakeUser,"helloworld");
+//   res.send(registerdUser);
+// });
 
-app.use("/listings", listings);
-app.use("/listings/:id/reviews", reviews);
+app.use("/listings", listingRouter);
+app.use("/listings/:id/reviews", reviewRouter);
+app.use("/",userRouter);
 
 //middleware to handle middleware
 
