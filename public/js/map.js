7e5d23055cb8e624017f42f5c4ca46895dd1e3de
Dnyaneshwar <dnyaneshwarmali0102@gmail.com>
2026-03-22 23:36:53 +0530

    added map marker to the location coordinates

--- public/js/map.js
+++ public/js/map.js
@@ -3,6 +3,9 @@ mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
   container: "map", // container ID
   style: "mapbox://styles/mapbox/streets-v12", // style URL
-  center: [-74.5, 40], // starting position [lng, lat]
+  center: coordinates, // starting position [lng, lat]
   zoom: 9, // starting zoom
 });
+const marker = new mapboxgl.Marker({color:"red"})
+.setLngLat(coordinates). // listing .geometry .coordinates
+addTo(map);
